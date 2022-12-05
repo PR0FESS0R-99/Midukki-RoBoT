cb0c970220f66ddff90dc1b92ad37cda8dbc949c
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-12-05 23:15:02 +0530

    Or

--- Midukki/__main__.py
+++ Midukki/__main__.py
@@ -42,7 +42,7 @@ async def start_command(client: Midukki_RoboT, message: message()):
                 await message.reply(e)
 
     if message.text.startswith("/start muhammedrk"):
-        if Configs.AUTH_CHANNEL != 1:
+        if Configs.AUTH_CHANNEL:
             invite_link = await client.create_chat_invite_link(int(Configs.AUTH_CHANNEL))
             try:
                 user = await client.get_chat_member(Configs.AUTH_CHANNEL, user_ids)
