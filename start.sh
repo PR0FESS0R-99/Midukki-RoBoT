2ceac27fd78e5c81bc05d50a25454a487d548559
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-12-20 21:10:50 +0530

    .

---
+++ start.sh
@@ -0,0 +1,13 @@
+if [ -z $REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/PR0FESS0R-99/Midukki-RoBoT.git /Midukki-RoBoT
+else
+  echo "Cloning Custom Repo from $REPO "
+  git clone $UPSTREAM_REPO /Midukki-RoBoT
+fi
+cd /Midukki-RoBoT
+pip3 install -U -r requirements.txt
+echo "Starting Midukki-RoBoT Bot...."
+
+python3 -m Midukki
