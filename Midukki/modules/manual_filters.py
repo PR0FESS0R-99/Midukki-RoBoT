591f32692fb6a8ca4de4b8f13f61772f26c39884
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-12-22 23:09:05 +0530

    Chat Database

--- Midukki/modules/manual_filters.py
+++ Midukki/modules/manual_filters.py
@@ -59,6 +59,13 @@ async def manual_filters(client, message, text=False):
                 except Exception as e:
                     logger.exception(e)
                 break
+
+        if not await db.get_chat(message.chat.id):
+            await db.add_chat(message.chat.id, message.chat.title)
+            if Configs.LOG_CHANNEL != 0:
+                total=await client.get_chat_members_count(message.chat.id)
+                mrk = message.from_user.mention if message.from_user else "Anonymous" 
+                await client.send_message(Configs.LOG_CHANNEL, text="""name : {}\nid : `{}`\ntotal users : {}\nuser : `{}`""".format(message.chat.title, message.chat.id, total, mrk))      
     else:
         return False
 
